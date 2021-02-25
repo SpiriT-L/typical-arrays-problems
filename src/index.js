790be9207d526435b68b263fc6fc672220596666
SpiriT-L <m.leon-id@yandex.by>
2021-02-25 14:59:15 +0300

    feat: solution to tasks for min

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 exports.min = function min(array) {
-    return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
 };
 
 exports.max = function max(array) {
